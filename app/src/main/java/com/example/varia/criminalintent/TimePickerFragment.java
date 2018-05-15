b59b2c7fdfb491e6a23b2bc4d302f8a28611f0f4
Velichko Michael <velichko@mdcm.co>
2018-05-15 17:37:19 +0300

    hotfix

---
+++ app/src/main/java/com/example/varia/criminalintent/TimePickerFragment.java
@@ -0,0 +1,93 @@
+package com.example.varia.criminalintent;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class TimePickerFragment extends DialogFragment {
+
+    private static final String ARG_DATE = "time";
+    public static final String EXTRA_DATE = "com.example.varia.criminalintent.date";
+    TimePicker mTimePicker;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_time, null);
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int hours = calendar.get(Calendar.HOUR_OF_DAY);
+        int minutes = calendar.get(Calendar.MINUTE);
+
+        mTimePicker = view.findViewById(R.id.dialog_time_picker);
+        mTimePicker.setIs24HourView(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mTimePicker.setHour(hours);
+            mTimePicker.setMinute(minutes);
+        } else {
+            mTimePicker.setCurrentHour(hours);
+            mTimePicker.setCurrentMinute(minutes);
+        }
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.time_picker_title)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Calendar calendar = Calendar.getInstance();
+                        int hours = 0, minutes = 0;
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            hours = mTimePicker.getHour();
+                            minutes = mTimePicker.getMinute();
+                        } else {
+                            hours = mTimePicker.getCurrentHour();
+                            minutes = mTimePicker.getCurrentMinute();
+                        }
+                        calendar.set(Calendar.HOUR_OF_DAY, hours);
+                        calendar.set(Calendar.MINUTE, minutes);
+                        sendResult(Activity.RESULT_OK, calendar);
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .setView(view)
+                .create();
+    }
+
+    public static TimePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        TimePickerFragment fragment = new TimePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    private void sendResult(int resultCode, Calendar calendar) {
+        if(getTargetFragment() == null)
+            return;
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE, calendar.getTime());
+        getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, intent);
+    }
+}
